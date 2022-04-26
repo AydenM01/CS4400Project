e07f4563affb0f21ff0b7c41056c98ce5f3117db
AydenM01 <aydenwilliammarshall@gmail.com>
2022-04-26 06:33:46 -0400

    Update _app.js

--- bank-management-ui/pages/_app.js
+++ bank-management-ui/pages/_app.js
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
+import AppContext from "../AppContext";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [userData, setUserData] = useState({ userID: "", userRole: "" });
+
+  return (
+    <AppContext.Provider
+      value={{
+        userData,
+        setUserData,
+      }}
+    >
+      <Component {...pageProps} userData={userData} setUserData={setUserData} />
+    </AppContext.Provider>
+  );
 }
 
 export default MyApp;
