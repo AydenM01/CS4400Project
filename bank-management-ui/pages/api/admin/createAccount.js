84117da1393a4a71fe0ca51af8586bc8f489e7c9
Ayden Marshall <aydenwilliammarshall@gmail.com>
2022-05-04 00:40:21 -0400

    createAccount API start

--- bank-management-ui/pages/api/admin/createAccount.js
+++ bank-management-ui/pages/api/admin/createAccount.js
@@ -1,13 +1,26 @@
 import { connection } from "../../lib/db";
 
 export default function handler(req, res) {
+  let requester = null;
+  let customer = null;
+  let account_type = null;
   let bankID = null;
-  let accountID = null;
   let balance = 0;
-
+  let interestRate = 0
+  let minBalance = 0;
+  let numWithdrawals = 0;
+  let maxWithdrawals = 0;
 
   if (req.method === "POST") {
+    req.body.requester ? (requester = '"' + req.body.requester + '"') : (requester = null);
+    req.body.customer ? (customer = '"' + req.body.customer + '"') : (customer = null);
+    req.body.account_type ? (account_type = '"' + req.body.account_type + '"') : (account_type = null);
     req.body.bankID ? (bankID = '"' + req.body.bankID + '"') : (bankID = null);
+    balance = req.body.balance;
+    interestRate = req.body.interestRate;
+    minBalance = req.body.minBalance;
+    numWithdrawals = req.body.numWithdrawals;
+    maxWithdrawals = req.body.maxWithdrawals;
     req.body.accountID ? (accountID = '"' + req.body.accountID + '"') : (accountID = null);
     balance = req.body.balance;
   }
