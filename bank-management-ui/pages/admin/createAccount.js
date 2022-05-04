7be8a16356b057f0f32877603114d89696ae0d06
AydenM01 <aydenwilliammarshall@gmail.com>
2022-05-04 01:55:02 -0400

    Create Account Admin

--- bank-management-ui/pages/admin/createAccount.js
+++ bank-management-ui/pages/admin/createAccount.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AppContext from "../../AppContext";
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 import { url } from "../../lib/env";
@@ -22,34 +23,72 @@ export async function getServerSideProps() {
 
   let personInit = personIDsJSON[0]["bankID"];
 
+  const customerIDs = await fetch(url + `/api/getCustomerIDs`);
+  const customerIDsJSON = await customerIDs.json();
+
+  let customerInit = customerIDsJSON[0]["perID"];
+
   // Pass data to the page via props
   return {
-    props: { personIDsJSON, personInit },
+    props: { personIDsJSON, personInit, customerIDsJSON, customerInit },
   };
 }
 
 export default function createAccount(props) {
   const [person, setPerson] = useState(props.personInit);
+  const [customer, setCustomer] = useState(props.customerInit);
+  const [accountType, setAccountType] = useState("checking");
+  const [accountID, setAccountID] = useState("");
   const [initialBalance, setInitialBalance] = useState(0);
   const [interestRate, setInterestRate] = useState(0);
   const [minBalance, setMinBalance] = useState(0);
   const [maxWithdrawals, setMaxWithdrawals] = useState(0);
+  const { userData, setUserData } = useContext(AppContext);
 
   const handleCreate = async () => {
     if (initialBalance < 0) {
-      alert("Balance Cannot be Negative");
+      alert("Initial Balance Cannot be Negative");
       return;
     }
-    const rawResponse = await fetch(url + "/api/createAccount", {
+
+    if (accountID === "") {
+      alert("Account ID cannot be empty");
+      return;
+    }
+
+    if (
+      interestRate <= 0 &&
+      (accountType === "savings" || accountType === "market")
+    ) {
+      alert("Interest Bearing Account must have nonzero positive interest");
+      return;
+    }
+
+    if (maxWithdrawals <= 0 && accountType === "market") {
+      alert("maxWithdrawals must be positive nonzero in Market Account");
+      return;
+    }
+
+    if (minBalance < 0 && accountType === "savings") {
+      alert("minBalance cannot be negative in Savings Account");
+      return;
+    }
+    const rawResponse = await fetch(url + "/api/admin/createAccount", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        bankID: bankID,
+        requester: userData.userID,
+        customer: customer,
+        account_type: accountType,
         accountID: accountID,
+        bankID: person,
         balance: initialBalance,
+        interestRate: interestRate,
+        minBalance: minBalance,
+        maxWithdrawals: maxWithdrawals,
       }),
     });
     const response = await rawResponse.json();
@@ -77,13 +116,33 @@ export default function createAccount(props) {
         <Grid item xs={2} />
         <Grid item xs={4}>
           <FormControl fullWidth>
-            <InputLabel>Person</InputLabel>
+            <InputLabel>Bank</InputLabel>
             <Select
-              label="Person"
-              value={"" + person}
+              label="Bank"
+              value={person}
               onChange={(e) => setPerson(e.target.value)}
             >
               {props.personIDsJSON.map((obj, i) => {
+                let perID = obj["bankID"];
+                return (
+                  <MenuItem key={i} value={perID}>
+                    {perID}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={4}>
+          <FormControl fullWidth>
+            <InputLabel>Customer</InputLabel>
+            <Select
+              label="Customer"
+              value={customer}
+              onChange={(e) => setCustomer(e.target.value)}
+            >
+              {props.customerIDsJSON.map((obj, i) => {
                 let perID = obj["perID"];
                 return (
                   <MenuItem key={i} value={perID}>
@@ -94,6 +153,39 @@ export default function createAccount(props) {
             </Select>
           </FormControl>
         </Grid>
+        <Grid item xs={2} />
+        <Grid item xs={2} />
+
+        <Grid item xs={4}>
+          <FormControl fullWidth>
+            <InputLabel>Account Type</InputLabel>
+            <Select
+              label="Account Type"
+              value={accountType}
+              onChange={(e) => setAccountType(e.target.value)}
+            >
+              {["checking", "savings", "market"].map((obj, i) => {
+                return (
+                  <MenuItem key={i} value={obj}>
+                    {obj}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={4}>
+          <TextField
+            label="Account ID"
+            fullWidth
+            value={accountID}
+            onChange={(e) => setAccountID(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={2} />
+
+        <Grid item xs={2} />
         <Grid item xs={4}>
           <TextField
             label="Initial Balance"
@@ -103,9 +195,7 @@ export default function createAccount(props) {
             onChange={(e) => setInitialBalance(e.target.value)}
           />
         </Grid>
-        <Grid item xs={2} />
 
-        <Grid item xs={2} />
         <Grid item xs={4}>
           <TextField
             label="Interest Rate"
@@ -113,8 +203,13 @@ export default function createAccount(props) {
             fullWidth
             value={interestRate}
             onChange={(e) => setInterestRate(e.target.value)}
+            disabled={accountType === "checking"}
           />
         </Grid>
+
+        <Grid item xs={2} />
+
+        <Grid item xs={2} />
         <Grid item xs={4}>
           <TextField
             label="Min Balance"
@@ -122,12 +217,10 @@ export default function createAccount(props) {
             fullWidth
             value={minBalance}
             onChange={(e) => setMinBalance(e.target.value)}
+            disabled={accountType === "checking" || accountType === "market"}
           />
         </Grid>
-        <Grid item xs={2} />
 
-
-        <Grid item xs={4} />
         <Grid item xs={4}>
           <TextField
             label="Max Withdrawals"
@@ -135,11 +228,10 @@ export default function createAccount(props) {
             fullWidth
             value={maxWithdrawals}
             onChange={(e) => setMaxWithdrawals(e.target.value)}
+            disabled={accountType === "checking" || accountType === "savings"}
           />
         </Grid>
-        <Grid item xs={4} />
-
-
+        <Grid item xs={2} />
 
         <Grid item xs={2} />
 
